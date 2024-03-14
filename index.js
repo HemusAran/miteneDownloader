5f67085d9c896e6ce757e222960b7f5195af447e
HemusAran <niconamachecker.hemus@gmail.com>
2024-03-14 10:31:40 +0900

    CDATA 自動設定
    サブ画面から CDATA を自動設定する仕組みを追加

--- index.js
+++ index.js
@@ -1,46 +1,22 @@
-// Modules to control application life and create native browser window
 const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 const path = require('path')
 const fs = require('fs')
-//const https = require('https')
 const { download } = require('electron-dl');
+const ini = require('ini');
 
 let mainWindow;
+let subWindow;
 
-function createWindow () {
-	// Create the browser window.
-	mainWindow = new BrowserWindow({
-		width: 800,
-		height: 800,
-		webPreferences: {
-			preload: path.join(__dirname, 'preload.js') 
-		}
-	});
+let package;
+let config;
 
-	mainWindow.setMenuBarVisibility(false);
-
-	const package_name = "Mitene Downloader";
-	const package_version = "1.5.0";
-	const title = package_name + " " + package_version;
-	mainWindow.setTitle(title);
-
-	// and load the index.html of the app.
-	mainWindow.loadFile('index.html');
-
-	// Open the DevTools.
-//	mainWindow.webContents.openDevTools();
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
 	createWindow();
 
 	app.on('activate', function () {
-	// On macOS it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (BrowserWindow.getAllWindows().length === 0) createWindow()
+		// On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
 	});
 })
 
@@ -51,12 +27,119 @@ app.on('window-all-closed', function () {
 	if (process.platform !== 'darwin') app.quit();
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// メイン画面の表示
+function createWindow () {
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 800,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js') 
+		},
+		show: false 
+	});
 
+	readPackageFile();
+	readConfigUrl();
+	
+	mainWindow.setTitle(package.name + " " + package.version);
 
+	mainWindow.setMenuBarVisibility(false);
 
+	mainWindow.loadFile('index.html');
 
+	mainWindow.webContents.on('did-finish-load', ()=>{
+		if (subWindow) {
+			// サブ画面を開いている場合は閉じる
+			subWindow.close();
+			subWindow = null;
+		}
+		
+		mainWindow.show();
+	});
+
+	// develop
+	//mainWindow.webContents.openDevTools();
+}
+
+// package.jsonの読み込み
+function readPackageFile() {
+	const filePath = path.join(__dirname, 'package.json');
+
+	try {
+		const fileData = fs.readFileSync(filePath, 'utf-8');
+		const data = JSON.parse(fileData);
+		package = data;
+	} catch (err) {
+		console.error('Error reading file:', err);
+	}
+}
+
+// config.iniの読み込み、URLを返す
+function readConfigUrl() {
+	const filePath = path.join(path.resolve(), 'config.ini');
+
+	try {
+		const fileData = fs.readFileSync(filePath, 'utf-8');
+		const data = ini.parse(fileData);
+		config = data;
+	} catch (err) {
+		console.error('Error reading file:', err);
+	}
+}
+
+// サブ画面の表示
+function createSubWindow(url) {
+	if (subWindow) {
+		// サブ画面を開いている場合は閉じる
+		subWindow.close();
+		subWindow = null;
+	}
+
+	subWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		x: mainWindow.getBounds().x + 600,
+		y: mainWindow.getBounds().y + 100,
+		parent: mainWindow,
+		show: false 
+	});
+	
+	subWindow.setMenuBarVisibility(false);
+	
+	subWindow.loadURL(url);
+	
+	subWindow.once('ready-to-show', () => {
+		subWindow.show();
+	});
+}
+
+// サブ画面にurlを渡して表示
+ipcMain.on('show-subwindow', async (event, url) => {
+	await createSubWindow(url);
+});
+
+// サブ画面にurlを渡して表示
+ipcMain.on('get-subwindow-html', (event, url) => {
+	subWindow.webContents.executeJavaScript(`
+		document.documentElement.outerHTML;
+	`).then((html) => {
+		let subWindowSrc = html;
+		event.reply('subwindow-html', subWindowSrc);
+	}).catch((error) => {
+		console.error('HTMLの取得中にエラーが発生しました:', error);
+	});
+});
+
+// メイン画面にurlを渡す
+ipcMain.on('get-config-url', (event) => {
+	if (config) {
+		if (config.general.url) {
+			event.reply('setConfigUrl', config.general.url);
+		}
+	}
+});
+
+// ダウンロード処理
 ipcMain.on('downloadAll', async (event, data) => {
 
 	// ダウンロード先のフォルダを選択するダイアログを表示
